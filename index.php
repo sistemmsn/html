1bf09735a3b2d3ee33fb1dd683b0e21d234d248f
sistemmsn <38023703+sistemmsn@users.noreply.github.com>
2018-04-04 21:27:28 -0500

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,6 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
         <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-        <script type="text/javascript" src="js/animation.js"></script>
       <div id="login-button">
         <img src="https://dqcgrsy5v35b9.cloudfront.net/cruiseplanner/assets/img/icons/login-w-icon.png"></img>
       </div>
@@ -42,5 +41,6 @@
          <a href="#" class="orange-btn">Get new password</a>
        </form>
       </div>-->
+      <script type="text/javascript" src="js/animation.js"></script>
     </body>
 </html>
